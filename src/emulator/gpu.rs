dcafc4ffb31914ee2f7230d64aadd1735a845afd
NivenT <nachenjang@gmail.com>
2016-06-07 21:28:57 -0400

    Added more gpu code

--- src/emulator/gpu.rs
+++ src/emulator/gpu.rs
@@ -4,20 +4,40 @@ const SCANLINE_TOTAL_TIME: i16 = 456;
 const SCANLINE_MODE2_OVER: i16 = 456-80;
 const SCANLINE_MODE3_OVER: i16 = 456-80-172;
 
+#[allow(non_camel_case_types)]
+#[derive(Debug, Clone, Copy)]
+pub enum Color{WHITE = 255, LIGHT_GRAY = 192, DARK_GRAY = 96, BLACK = 0}
+
+impl Color {
+	fn from_palette(id: u8, palette: u8) -> Color {
+		let (hi, lo) = (2*id+1, 2*id);
+		let color = ((palette & (1 << hi)) >> (hi-1)) | ((palette & (1 << lo)) >> lo);
+		match color {
+			0 => Color::WHITE,
+			1 => Color::LIGHT_GRAY,
+			2 => Color::DARK_GRAY,
+			3 => Color::BLACK,
+			_ => panic!("Invalid color: {}", color)
+		}
+	}
+}
+
 #[allow(dead_code)]
 pub struct Gpu {
+	//Screen is 160x144 pixels
+	screen_data:	[[Color; 160]; 144],
 	//scanline counter
 	sl_count:		i16,
-	//Scroll Registers
-	scx: 			u8,
-	scy: 			u8,
 	//Background Palette Register
 	bp:				u8,
 }
 
 impl Gpu {
 	pub fn new() -> Gpu {
-	    Gpu{sl_count: 0, scx: 0, scy: 0, bp: 0}
+	    Gpu{screen_data: [[Color::BLACK; 160]; 144], sl_count: 0, bp: 0}
+	}
+	pub fn get_screen(&self) -> &[[Color; 160]; 144] {
+		&self.screen_data
 	}
 	pub fn step(&mut self, mem: &mut Memory, cycles: i16) {
 		self.set_lcd_status(mem);
@@ -31,7 +51,7 @@ impl Gpu {
 				if line == 144 {
 					//Request Interupt
 				} else if line < 144 {
-					//Draw the line
+					self.draw_line(mem);
 				}
 			}
 		}
@@ -75,5 +95,59 @@ impl Gpu {
 	}
 	fn is_lcd_enabled(&self, mem: &Memory) -> bool {
 		(mem.rb(0xFF40) & (1 << 7)) > 0
+	}
+
+	fn draw_line(&mut self, mem: &mut Memory) {
+		let control = mem.rb(0xFF40);
+		if (control & 1) > 0 {
+			self.draw_background(mem);
+		}
+		if (control & 2) > 0 {
+			self.draw_sprites(mem);
+		}
+	}
+	fn draw_background(&mut self, mem: &mut Memory) {
+		let (scroll_y, scroll_x) = (mem.rb(0xFF42), mem.rb(0xFF43));
+		let (window_y, window_x) = (mem.rb(0xFF4A), mem.rb(0xFF4B) - 7);
+
+		let control = mem.rb(0xFF40);
+		let tile_data_loc   = if (control & (1 << 4)) > 0 {0x8000} else {0x8800};
+		let back_layout_loc = if (control & (1 << 3)) > 0 {0x9C00} else {0x9800};
+		let wind_layout_loc = if (control & (1 << 6)) > 0 {0x9C00} else {0x9800};
+		let using_window = (control & (1 << 5)) > 0 && window_y <= mem.rb(0xFF44);
+		let background_loc: u16 = if using_window {wind_layout_loc} else {back_layout_loc};
+
+		let line = mem.rb(0xFF44);
+		let y_offset = if using_window {line - window_y} else {scroll_y + line};
+		let tile_row = y_offset/8;
+
+		for pixel in 0..160 {
+			let mut x_offset = pixel + scroll_x;
+			if using_window && pixel >= window_x {
+				x_offset = pixel - window_x;
+			}
+
+			let tile_col = x_offset/8;
+			let address = background_loc + tile_row as u16 *32 + tile_col as u16;
+
+			let tile_loc = if tile_data_loc == 0x8000 {
+				tile_data_loc + (mem.rb(address as u16) as u16)*16
+			} else {
+				tile_data_loc + ((mem.rb(address as u16) as i16 + 128) as u16 * 16)
+			};
+			let tile_line = (y_offset%8) as u16;
+
+			let tile_data = [mem.rb(tile_loc + tile_line*2), mem.rb(tile_loc + tile_line*2 + 1)];
+			let color_bit = 7 - x_offset%8;
+			let color_id = ((tile_data[1] & (1 << color_bit)) >> (color_bit-1)) | 
+						   ((tile_data[0] & (1 << color_bit)) >> color_bit);
+
+			self.screen_data[line as usize][pixel as usize] = 
+				Color::from_palette(color_id, mem.rb(0xFF47));
+		}
+	}
+	#[allow(unused_variables)]
+	fn draw_sprites(&mut self, mem: &mut Memory) {
+
 	}
 }
